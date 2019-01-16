gandrewstone/BitcoinUnlimited

convert wallet to use group authorities

src/wallet/wallet.cpp
@@ -2653,6 +2653,7 @@ bool CWallet::CommitTransaction(CWalletTx &wtxNew, CReserveKey &reservekey)
     {
         LOCK2(cs_main, cs_wallet);
         LOGA("CommitTransaction:\n%s", wtxNew.ToString());
+        LOGA("CommitTransaction:\n%s\n", EncodeHexTx(static_cast<CTransaction>(wtxNew)));
 
 #if 1
         if (fBroadcastTransactions)

FornaxA/ion

src/wallet.cpp
@@ -13,6 +13,7 @@
 #include "checkpoints.h"
 #include "coincontrol.h"
 #include "consensus/tokengroups.h"
+#include "core_io.h"
 #include "kernel.h"
 #include "masternode-budget.h"
 #include "net.h"
@@ -3525,6 +3526,7 @@ bool CWallet::CommitTransaction(CWalletTx& wtxNew, CReserveKey& reservekey, std:
     {
         LOCK2(cs_main, cs_wallet);
         LogPrintf("CommitTransaction:\n%s", wtxNew.ToString());
+        LogPrintf("CommitTransaction:\n%s\n", EncodeHexTx(static_cast<CTransaction>(wtxNew)));
         {
             // This is only to keep the database open to defeat the auto-flush for the
             // duration of this scope.  This is the only place where this optimization