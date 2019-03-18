077ca00a36aa7e5e529ff03d96ed248216950683
FornaxA <fornaxa@servitising.org>
2019-03-18 18:00:20 +0100

    RPC command for querying dark matter transaction count info

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -446,7 +446,7 @@ static const CRPCCommand vRPCCommands[] =
         {"wallet", "walletpassphrase", &walletpassphrase, true, false, true},
         {"wallet", "token", &token, false, false, true},
         {"wallet", "managementtoken", &managementtoken, false, false, true},
-        {"wallet", "tokendescription", &tokendescription, false, false, true},
+        {"wallet", "tokeninfo", &tokeninfo, false, false, true},
 
         {"zerocoin", "getzerocoinbalance", &getzerocoinbalance, false, false, true},
         {"zerocoin", "listmintedzerocoins", &listmintedzerocoins, false, false, true},
