jcramer/BitcoinUnlimited

add token-only listsinceblock and listtransactions functionality

src/wallet/wallet.cpp
@@ -1229,6 +1229,72 @@ void CWalletTx::GetAmounts(list<COutputEntry> &listReceived,
     }
 }
 
+void CWalletTx::GetGroupAmounts(const CTokenGroupID& grp, list<COutputEntry> &listReceived,
+    list<COutputEntry> &listSent,
+    CAmount &nFee,
+    string &strSentAccount,
+    const isminefilter &filter) const
+{
+    nFee = 0;
+    listReceived.clear();
+    listSent.clear();
+    strSentAccount = strFromAccount;
+
+    // Compute fee:
+    CAmount nDebit = GetDebit(filter);
+    if (nDebit > 0) // debit>0 means we signed/sent this transaction
+    {
+        CAmount nValueOut = GetValueOut();
+        nFee = nDebit - nValueOut;
+    }
+
+    // Sent/received.
+    for (unsigned int i = 0; i < vout.size(); ++i)
+    {
+        const CTxOut &txout = vout[i];
+        isminetype fIsMine = pwallet->IsMine(txout);
+        // Only need to handle txouts if AT LEAST one of these is true:
+        //   1) they debit from us (sent)
+        //   2) the output is to us (received)
+        if (nDebit > 0)
+        {
+            // Don't report 'change' txouts
+            if (pwallet->IsChange(txout))
+                continue;
+        }
+        else if (!(fIsMine & filter))
+            continue;
+
+        // In either case, we need to get the destination address
+        CTxDestination address;
+        txnouttype whichType;
+        if (!ExtractDestinationAndType(txout.scriptPubKey, address, whichType) && !txout.scriptPubKey.IsUnspendable())
+        {
+            LOGA("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n", this->GetHash().ToString());
+            address = CNoDestination();
+        }
+
+        // Only return group outputs from this API
+        if ((whichType == TX_GRP_PUBKEYHASH) || (whichType == TX_GRP_SCRIPTHASH))
+        {
+            CTokenGroupInfo txgrp(txout.scriptPubKey);
+            if (grp == txgrp.associatedGroup)
+            {
+                COutputEntry output = {address, txgrp.quantity, (int)i};
+
+                // If we are debited by the transaction, add the output as a "sent" entry
+                if (nDebit > 0)
+                    listSent.push_back(output);
+
+                // If we are receiving the output, add it as a "received" entry
+                if (fIsMine & filter)
+                    listReceived.push_back(output);
+            }
+        }
+    }
+}
+
+
 void CWalletTx::GetAccountAmounts(const string &strAccount,
     CAmount &nReceived,
     CAmount &nSent,

FornaxA/ion

src/wallet/wallet.cpp
@@ -1418,6 +1418,72 @@ void CWalletTx::GetAmounts(list<COutputEntry>& listReceived,
     }
 }
 
+void CWalletTx::GetGroupAmounts(const CTokenGroupID& grp, list<COutputEntry> &listReceived,
+    list<COutputEntry> &listSent,
+    CAmount &nFee,
+    string &strSentAccount,
+    const isminefilter &filter) const
+{
+    nFee = 0;
+    listReceived.clear();
+    listSent.clear();
+    strSentAccount = strFromAccount;
+
+    // Compute fee:
+    CAmount nDebit = GetDebit(filter);
+    if (nDebit > 0) // debit>0 means we signed/sent this transaction
+    {
+        CAmount nValueOut = GetValueOut();
+        nFee = nDebit - nValueOut;
+    }
+
+    // Sent/received.
+    for (unsigned int i = 0; i < vout.size(); ++i)
+    {
+        const CTxOut &txout = vout[i];
+        isminetype fIsMine = pwallet->IsMine(txout);
+        // Only need to handle txouts if AT LEAST one of these is true:
+        //   1) they debit from us (sent)
+        //   2) the output is to us (received)
+        if (nDebit > 0)
+        {
+            // Don't report 'change' txouts
+            if (pwallet->IsChange(txout))
+                continue;
+        }
+        else if (!(fIsMine & filter))
+            continue;
+
+        // In either case, we need to get the destination address
+        CTxDestination address;
+        txnouttype whichType;
+        if (!ExtractDestinationAndType(txout.scriptPubKey, address, whichType) && !txout.scriptPubKey.IsUnspendable())
+        {
+            LogPrintf("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n", this->GetHash().ToString());
+            address = CNoDestination();
+        }
+
+        // Only return group outputs from this API
+        if ((whichType == TX_GRP_PUBKEYHASH) || (whichType == TX_GRP_SCRIPTHASH))
+        {
+            CTokenGroupInfo txgrp(txout.scriptPubKey);
+            if (grp == txgrp.associatedGroup)
+            {
+                COutputEntry output = {address, txgrp.quantity, (int)i};
+
+                // If we are debited by the transaction, add the output as a "sent" entry
+                if (nDebit > 0)
+                    listSent.push_back(output);
+
+                // If we are receiving the output, add it as a "received" entry
+                if (fIsMine & filter)
+                    listReceived.push_back(output);
+            }
+        }
+    }
+}
+
+
 void CWalletTx::GetAccountAmounts(const string& strAccount, CAmount& nReceived, CAmount& nSent, CAmount& nFee, const isminefilter& filter) const
 {
     nReceived = nSent = nFee = 0;